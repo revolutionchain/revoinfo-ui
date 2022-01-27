c8bdf2519d2ce6d557bcf81cf822c5e11f374518
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2022-01-27 15:12:18 +0100

    Update Version Name

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ import webpack from 'webpack'
 
 export default {
   head: {
-    titleTemplate: '%s - revo.info',
+    titleTemplate: '%s - Beta - Revo Explorer',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1, user-scalable=no'}
