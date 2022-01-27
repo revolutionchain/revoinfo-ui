8c19ffee425e7bf5fd0b51be27340279090acc50
RevolutionChain Italy <62940168+revolutionchain@users.noreply.github.com>
2022-01-27 12:37:32 +0100

    Add Misc Icons

--- pages/misc.vue
+++ pages/misc.vue
@@ -3,19 +3,19 @@
     <div class="tabs is-centered">
       <ul>
         <li :class="{'is-active': $route.matched.some(route => route.name === 'misc-charts')}">
-          <nuxt-link to="/misc/charts">{{ $t('misc.charts_title') }}</nuxt-link>
+          <nuxt-link to="/misc/charts"><Icon icon="fas chart-bar" fixedWidth /> {{ $t('misc.charts_title') }}</nuxt-link>
         </li>
         <li :class="{'is-active': $route.matched.some(route => route.name === 'misc-rich-list')}">
-          <nuxt-link to="/misc/rich-list">{{ $t('misc.rich_list_title') }}</nuxt-link>
+          <nuxt-link to="/misc/rich-list"><Icon icon="trophy" fixedWidth /> {{ $t('misc.rich_list_title') }}</nuxt-link>
         </li>
         <li :class="{'is-active': $route.matched.some(route => route.name === 'misc-biggest-miners')}">
-          <nuxt-link to="/misc/biggest-miners">{{ $t('misc.biggest_miners_title') }}</nuxt-link>
+          <nuxt-link to="/misc/biggest-miners"><Icon icon="fas meteor" fixedWidth /> {{ $t('misc.biggest_miners_title') }}</nuxt-link>
         </li>
         <li :class="{'is-active': $route.matched.some(route => route.name === 'misc-stake-calculator')}">
-          <nuxt-link to="/misc/stake-calculator">{{ $t('misc.stake_calculator.title') }}</nuxt-link>
+          <nuxt-link to="/misc/stake-calculator"><Icon icon="fas calculator" fixedWidth /> {{ $t('misc.stake_calculator.title') }}</nuxt-link>
         </li>
         <li :class="{'is-active': $route.matched.some(route => route.name === 'misc-raw-tx')}">
-          <nuxt-link to="/misc/raw-tx">Send Raw Transaction</nuxt-link>
+          <nuxt-link to="/misc/raw-tx"><Icon icon="fas paper-plane" fixedWidth />Send Raw Transaction</nuxt-link>
         </li>
       </ul>
     </div>
